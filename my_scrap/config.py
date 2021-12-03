86065b3917cbfb60a0423d8a3810b067683ae325
umbrachorum <quentin1.vedrenne@epitech.eu>
2021-12-03 13:12:31 +0100

    fix: json convert

--- my_scrap/config.py
+++ my_scrap/config.py
@@ -3,9 +3,10 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    def __init__(self, url, navigator, key_word, element, nb_elem):
+    def __init__(self, url, navigator, key_word, element, nb_elem, page_nb):
         self.url = url
         self.key_word = key_word
         self.navigator = navigator
         self.element = element
         self.nb = nb_elem
+        self.page_nb = page_nb
