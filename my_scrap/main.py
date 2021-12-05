36d0b9ceb598492fd3210b4537002aea15c168cb
umbrachorum <quentin1.vedrenne@epitech.eu>
2021-12-05 15:40:26 +0100

    clear

--- my_scrap/main.py
+++
@@ -1,7 +0,0 @@
-from Scrap import *
-from config import *
-
-if __name__ == '__main__':
-    listOfElem = ["enter", "your", "option"]
-    conf = Config("test", "chrome", "iphone 11", listOfElem, 50, 6)
-    my_open(conf)
